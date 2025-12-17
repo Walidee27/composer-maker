406e6edb098c040f03924bd0321790be6df5da93
Walide Ghazanfar <walide@MacBook-Pro-de-Walide.local>
2025-12-17 14:46:45 +0100

    Quatrième commit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ShopController extends AbstractController
+final class AdminController extends AbstractController
 {
-    #[Route('/shop', name: 'app_product')]
+    #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('shop/index.html.twig', [
-            'controller_name' => 'ProductController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
