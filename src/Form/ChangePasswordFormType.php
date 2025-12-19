b859d384422bbe66b62f7a951c707055fc38f1aa
Walide Ghazanfar <walide7230@gmail.com>
2025-12-19 12:33:32 +0100

    Commit avec le .env

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -32,7 +32,6 @@ class ChangePasswordFormType extends AbstractType
                         new Length([
                             'min' => 12,
                             'minMessage' => 'Your password should be at least {{ limit }} characters',
-                            // max length allowed by Symfony for security reasons
                             'max' => 4096,
                         ]),
                         new PasswordStrength(),
